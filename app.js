80387a081d93672318cc1ef691ec2d270e0fa1e7
“zvhao” <nguyenvanhao29102001@gmail.com>
2023-02-05 11:21:02 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const contactsRouter = require("./app/routes/contact.route")
 
 const app = express()
 
@@ -9,5 +10,7 @@ app.use(express.json())
 app.get("/", (req, res) => {
 	res.json({message: "Welcome to contact book application."})
 })
+
+app.use("/api/contacts", contactsRouter)
 
 module.exports = app
